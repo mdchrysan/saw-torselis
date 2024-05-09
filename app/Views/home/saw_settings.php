7273ba04a03cc383eb2c68f4dd6dd3025bf9f9ff
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2024-05-09 22:15:10 +0700

    fix settings layout

--- app/Views/home/saw_settings.php
+++ app/Views/home/saw_settings.php
@@ -5,39 +5,41 @@
     <div class="container p-5 text-center">
         <form action="/result" method="POST" enctype="multipart/form-data">
             <?= csrf_field(); ?>
-            <div class="m-5">
-                <h3>1. Pilih Jenis Kendaraan</h3>
-                <div class="container w-50">
+            <div class="container">
+                <h3 class="mt-5">1. Pilih Jenis Kendaraan</h3>
+                <div class="container setting-group">
                     <div class="row mb-3">
                         <!-- setting 1 -->
                         <div class="col-lg-6 col-md-12">
-                            <input type="radio" class="btn-check" name="type" id="btnMotor" autocomplete="off" value="1">
                             <label class="btn btn-lg btn-warning btn-outline-dark shadow" for="btnMotor">
-                                <img src="img/icons/motorcycle.png" alt="">
+                                <input type="radio" class="btn-check invisible-radio" name="type" id="btnMotor" autocomplete="off" value="1">
+                                <!-- <img src="img/icons/motorcycle.png" alt=""> -->
+                                <i class="fa-solid fa-motorcycle"></i>
                                 Motor Listrik
                             </label>
                         </div>
                         <!-- setting 2 -->
                         <div class="col-lg-6 col-md-12">
-                            <input type="radio" class="btn-check" name="type" id="btnSepeda" autocomplete="off" value="2">
                             <label class="btn btn-lg btn-warning btn-outline-dark shadow" for="btnSepeda">
-                                <img src="img/icons/bicycle.png" alt="">
+                                <input type="radio" class="btn-check invisible-radio" name="type" id="btnSepeda" autocomplete="off" value="2">
+                                <!-- <img src="img/icons/bicycle.png" alt=""> -->
+                                <i class="fa-solid fa-bicycle"></i>
                                 Sepeda Listrik
                             </label>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="m-5">
-                <h3>2. Tentukan Pengaturan Tiap Kriteria</h3>
-                <div class="container w-50">
+            <div class="container">
+                <h3 class="mt-5">2. Tentukan Pengaturan Tiap Kriteria</h3>
+                <div class="container setting-group">
                     <!-- setting 1 -->
                     <div class="row mb-3 align-items-center">
                         <div class="col-lg-6 col-md-12">
                             <h6>Harga</h6>
                         </div>
                         <div class="col-lg-6 col-md-12">
-                            <select class="form-select shadow" name="w_c1">
+                            <select class="form-select select-big shadow" name="w_c1" required>
                                 <option selected>Pilih kriteria</option>
                                 <option value="4">Murah</option>
                                 <option value="3">Sedang</option>
@@ -52,7 +54,7 @@
                             <h6>Power</h6>
                         </div>
                         <div class="col-lg-6 col-md-12">
-                            <select class="form-select shadow" name="w_c2">
+                            <select class="form-select select-big shadow" name="w_c2" required>
                                 <option selected>Pilih kriteria</option>
                                 <option value="1">Kecil</option>
                                 <option value="2">Sedang</option>
@@ -67,7 +69,7 @@
                             <h6>Kecepatan Max</h6>
                         </div>
                         <div class="col-lg-6 col-md-12">
-                            <select class="form-select shadow" name="w_c3">
+                            <select class="form-select select-big shadow" name="w_c3" required>
                                 <option selected>Pilih kriteria</option>
                                 <option value="1">Lambat</option>
                                 <option value="2">Biasa</option>
@@ -82,7 +84,7 @@
                             <h6>Jarak Tempuh</h6>
                         </div>
                         <div class="col-lg-6 col-md-12">
-                            <select class="form-select shadow" name="w_c4">
+                            <select class="form-select select-big shadow" name="w_c4" required>
                                 <option selected>Pilih kriteria</option>
                                 <option value="1">Rendah</option>
                                 <option value="2">Sedang</option>
@@ -93,9 +95,9 @@
                     </div>
                 </div>
             </div>
-            <div class="m-5">
-                <h3>3. Klik Tombol</h3>
-                <button type="submit" class="btn btn-lg btn-warning shadow">Dapatkan Hasil</button>
+            <div class="container">
+                <h3 class="mt-5">3. Klik Tombol</h3>
+                <button type="submit" class="btn btn-lg btn-warning btn-hasil">Dapatkan Hasil</button>
             </div>
         </form>
     </div>
